3392c4fcec728e0784e47b9c1b7d52fe99dae56a
Ben Miller <ben.miller@isotoma.com>
2010-05-13 15:15:20 +0100

    Removes Sites model from admin

--- src/rietveld_helper/admin.py
+++ src/rietveld_helper/admin.py
@@ -1,26 +1,27 @@
 from django.contrib import admin
+from django.contrib.sites.models import Site
 
 from codereview import models
 
 # Patch in some simple lambda's, Django uses them.
-#models.Issue.__unicode__ = lambda self: self.subject
-#models.PatchSet.__unicode__ = lambda self: self.message or ''
+models.Issue.__unicode__ = lambda self: self.subject
+models.PatchSet.__unicode__ = lambda self: self.message or ''
 
+class PatchSetInlineAdmin(admin.TabularInline):
+    model = models.PatchSet
 
-#class PatchSetInlineAdmin(admin.TabularInline):
-#    model = models.PatchSet
+class PatchSetAdmin(admin.ModelAdmin):
+    list_filter = ('issue', 'owner')
+    list_display = ('issue', 'message')
+    search_fields = ('issue__subject', 'message')
 
-
-#class PatchSetAdmin(admin.ModelAdmin):
-#    list_filter = ('issue', 'owner')
-#    list_display = ('issue', 'message')
-#    search_fields = ('issue__subject', 'message')
-
-#class IssueAdmin(admin.ModelAdmin):
-#    list_filter = ('closed', 'owner')
-#    list_display = ('id', 'subject', 'owner', 'modified', 'n_comments')
-#    list_display_links = ('id', 'subject')
-#    inlines = [PatchSetInlineAdmin]
+class IssueAdmin(admin.ModelAdmin):
+    list_filter = ('closed', 'owner')
+    list_display = ('id', 'subject', 'owner', 'modified', 'n_comments')
+    list_display_links = ('id', 'subject')
+    inlines = [PatchSetInlineAdmin]
 
 #admin.site.register(models.Issue, IssueAdmin)
 #admin.site.register(models.PatchSet, PatchSetAdmin)
+
+admin.site.unregister(Site)
