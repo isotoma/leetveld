fe6a72983784e1ad63208fd85a1df90bf72e5902
Ben Miller <ben.miller@isotoma.com>
2010-10-11 10:08:53 +0100

    Bumping version to 0.7

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='leetveld',
-    version='0.6',
+    version='0.7',
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
