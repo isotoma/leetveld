7c83ffdccfebe3de6b6e3d6973b1b14077fd22f8
Alex Holmes <alex@alex-holmes.com>
2011-09-12 17:12:18 +0100

    Update the setup.py to point to leetveld, not codereview

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version_file = os.path.join(os.path.dirname(__file__), "codereview", "version.txt")
+version_file = os.path.join(os.path.dirname(__file__), "leetveld", "version.txt")
 
 __version__ = open(version_file).read().strip()
 
