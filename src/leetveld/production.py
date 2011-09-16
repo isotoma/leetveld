cef25a35dc8d07649402670a66a026d77703a572
Alex Holmes <alex@alex-holmes.com>
2011-09-16 03:58:10 +0100

    Update logging configuration

--- src/leetveld/production.py
+++ src/leetveld/production.py
@@ -13,7 +13,49 @@ DATABASES = {
     }
 }
 
-LOG_FILE = '/var/log/leetveld/leetveld.log'
+LOG_FILE = '/var/log/leetveld-production/leetveld.log'
+
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(name)s %(process)d %(message)s',
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s',
+        },
+    },
+    'handlers': {
+        'log_file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': LOG_FILE,
+            'formatter': 'verbose',
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['console'],
+            'propagate': True,
+            'level':'INFO',
+        },
+        'django.request': {
+            'handlers': ['log_file', 'console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'leetveld': {
+            'handlers': ['log_file'],
+            'level': 'INFO',
+            'propagate': True,
+        }
+    }
+}
 
 try:
     from private_settings import *
