e0080fac5adbd190318c8e9ba6e1d87a6e15fd2c
Alex Holmes <alex@alex-holmes.com>
2011-09-19 12:28:58 +0100

    Preparing for release 0.11.0

---
+++ CHANGES.rst
@@ -0,0 +1,9 @@
+Leetveld
+========
+
+0.11.0 (2011-09-19)
+-------------------
+
+- Complete update of the leetveld codebase, using the most up-to-date
+  django-gae2django and rietveld code, with isotoma-specific authentication
+  patches manually cherry-picked back in.
--- src/setup.py
+++ src/setup.py
@@ -1,9 +1,28 @@
 from setuptools import setup, find_packages
 
-version = '0.11.0dev'
+version = '0.11.0'
+
+def long_description():
+    def file_or_emptystring(filename):
+        contents = ''
+        try:
+            this_file = open(filename, 'r')
+            contents = this_file.read()
+            this_file.close()
+        except IOError:
+            pass
+
+        return contents
+
+    return '%s\n\n%s' % (
+        file_or_emptystring('../README.rst'),
+        file_or_emptystring('../CHANGES.rst')
+    )
 
 setup(
     name='leetveld',
+    description='A ready-to-deploy gae2django version of rietveld',
+    long_description=long_description(),
     version=version,
     packages=find_packages(),
     zip_safe=False,
